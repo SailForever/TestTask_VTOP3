c992c514cdf19d521f7bc71e55fb19f4243f6d9b
SailForever <ermolovich_artem@mail.ru>
2023-06-25 16:15:59 +0300

    File formatting, styles for desktop and final form validation check, mobile version coming soon

--- JS/form_valid.js
+++ JS/form_valid.js
@@ -4,17 +4,22 @@ const formFirstF = document.forms.firstF;
 //firstName
 const firstNameJs = formFirstF.elements.firstName;
 const firstNameJsValue = firstNameJs.value;
-firstNameJs.addEventListener('blur', (eo)=>firstNameBlur(false));
+firstNameJs.addEventListener('blur', (eo) => firstNameBlur(false));
 function firstNameBlur(focusError) {
   const formFirstF = document.forms.firstF;
   const firstNameJs = formFirstF.elements.firstName;
   const firstNameJsValue = firstNameJs.value;
+  const firstNameRedBorder = document.querySelector('.formText1');
   let errors = 0;
   if ((firstNameJsValue.length > 40) || (firstNameJsValue.length < 2)) {
-    firstNameJs.placeholder="Enter your First Name";
+    firstNameJs.placeholder = "Enter your First Name";
+    firstNameRedBorder.style.borderBottom = '2px solid #FF2828';
     errors++;
     if (focusError)
       firstNameJs.focus();
+  }
+  else {
+    firstNameRedBorder.style.borderBottom = '1px solid #F2F2F2';
   }
   return errors;
 }
@@ -22,17 +27,22 @@ function firstNameBlur(focusError) {
 //lastName
 const lastNameJs = formFirstF.elements.lastName;
 const lastNameJsValue = lastNameJs.value;
-lastNameJs.addEventListener('blur', (eo)=>lastNameBlur(false));
+lastNameJs.addEventListener('blur', (eo) => lastNameBlur(false));
 function lastNameBlur(focusError) {
   const formFirstF = document.forms.firstF;
   const lastNameJs = formFirstF.elements.lastName;
   const lastNameJsValue = lastNameJs.value;
+  const lastNameRedBorder = document.querySelector('.formText2');
   let errors = 0;
   if ((lastNameJsValue.length > 40) || (lastNameJsValue.length < 2)) {
-    lastNameJs.placeholder="Enter your Last Name";
+    lastNameJs.placeholder = "Enter your Last Name";
+    lastNameRedBorder.style.borderBottom = '2px solid #FF2828';
     errors++;
     if (focusError)
       lastNameJs.focus();
+  }
+  else {
+    lastNameRedBorder.style.borderBottom = '1px solid #F2F2F2';
   }
   return errors;
 }
@@ -40,16 +50,27 @@ function lastNameBlur(focusError) {
 //Nationality
 const nationalityJs = formFirstF.elements.nationality;
 const nationalityJsValue = nationalityJs.value;
-nationalityJs.addEventListener('change', (eo)=>nationalityChange(false));
+nationalityJs.addEventListener('blur', (eo) => nationalityChange(false));
+nationalityJs.addEventListener('change', (eo) => nationalityChange(false));
 function nationalityChange(focusError) {
   const formFirstF = document.forms.firstF;
   const nationalityJs = formFirstF.elements.nationality;
   const nationalityJsValue = nationalityJs.value;
+  const nationalityRedBorder = document.querySelector('.formText3');
+  const nationalitySelectColor = document.getElementById('selectColor');
+  const nationalityChooseNat = document.getElementById('chooseNat')
   let errors = 0;
   if (nationalityJsValue == 0) {
+    nationalityRedBorder.style.borderBottom = '2px solid #FF2828';
+    nationalitySelectColor.style.color = '#FF2828';
+    nationalityChooseNat.innerText = 'Choose your Nationality';
     errors++;
     if (focusError)
       nationalityJs.focus();
+  }
+  else {
+    nationalityRedBorder.style.borderBottom = '1px solid #F2F2F2';
+    nationalitySelectColor.style.color = '';
   }
   return errors;
 }
@@ -57,55 +78,70 @@ function nationalityChange(focusError) {
 //email
 const emailName = formFirstF.elements.email;
 const emailValue = emailName.value;
-emailName.addEventListener('blur', (eo)=>emailBlur(false));
+emailName.addEventListener('blur', (eo) => emailBlur(false));
 function emailBlur(focusError) {
   const formFirstF = document.forms.firstF;
   const emailName = formFirstF.elements.email;
   const emValid = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-  let errors=0;
+  const emailRedBorder = document.querySelector('.formText4');
+  let errors = 0;
   if (!(isEmailValid(emailName.value))) {
     emailName.placeholder = 'Enter your E-mail';
+    emailRedBorder.style.borderBottom = '2px solid #FF2828';
     errors++;
     if (focusError)
       emailName.focus();
   }
-    function isEmailValid(value) {
-      return emValid.test(value);
-    }
+  else {
+    emailRedBorder.style.borderBottom = '1px solid #F2F2F2';
+  }
+  function isEmailValid(value) {
+    return emValid.test(value);
+  }
   return errors;
 }
 
 //Date of Birth
 const dataName = formFirstF.elements.dateWeb;
 const dataValue = dataName.value;
-dataName.addEventListener('blur', (eo)=>dateBlur(false));
+dataName.addEventListener('blur', (eo) => dateBlur(false));
 function dateBlur(focusError) {
   const formFirstF = document.forms.firstF;
   const dataName = formFirstF.elements.dateWeb;
   const dataValue = dataName.value;
+  const dateChoose = document.getElementById('dateCh')
+  const dateRedBorder = document.querySelector('.formText5');
   let errors = 0;
-  if (dataValue.length<1 || (dataValue>'2024-01-01') || (dataValue<'1900-01-01')) {
+  if (dataValue.length < 1 || (dataValue > '2024-01-01') || (dataValue < '1900-01-01')) {
     dataName.placeholder = 'Enter your Date of Birth';
+    dateChoose.style.color = '#FF2828';
+    dateRedBorder.style.borderBottom = '2px solid #FF2828';
     errors++;
     if (focusError)
       dataName.focus();
+  }
+  else {
+    dateChoose.style.color = '';
+    dateRedBorder.style.borderBottom = '1px solid #F2F2F2';
   }
   return errors;
 }
 
 //Gender
 var elemsRadio = formFirstF.elements.gender;
-const elemsRadioValue=elemsRadio.value;
+const elemsRadioValue = elemsRadio.value;
 for (var i = elemsRadio.length - 1; i >= 0; i--) {
   const elemRadio = elemsRadio[i];
-  elemRadio.addEventListener('click', ()=>validAnswer(elemRadio));
+  elemRadio.addEventListener('click', () => validAnswer(elemRadio));
 };
-var errorsRadio= 0;
-var bNameRadio = document.getElementById('nameRadioFail');
-function validAnswer (elemRadio) {
+var errorsRadio = 0;
+function validAnswer(elemRadio) {
   if (elemRadio.checked == true) {
     errorsRadio++;
-    bNameRadio.innerHTML = '';
+    const radioColor1 = document.querySelector('.radio1Txt')
+    const radioColor2 = document.querySelector('.radio2Txt')
+    radioColor1.style.color = '';
+    radioColor2.style.color = '';
   }
   return errorsRadio;
 }
@@ -118,7 +154,7 @@ function passwordJsBlur(focusError) {
   const formFirstF = document.forms.firstF;
   const passwordJs = formFirstF.elements.password;
   passwordJsValue = passwordJs.value;
-  let bName = document.getElementById('namePasswordFail');
+  const passwordBorder = document.querySelector('.formText7');
   let uppers = /[A-Z]/; // There is at least one letter in upper case
   let lowers = /[a-z]/; // There is at least one letter in lower case
   let numbers = /\d/; // There is at least one figure
@@ -126,13 +162,15 @@ function passwordJsBlur(focusError) {
   let errors = 0;
 
   if (!(isPasswordUpper(passwordJs.value)) || !(isPasswordLowers(passwordJs.value)) || !(isPasswordNumbers(passwordJs.value)) || !(isPasswordOnlylatin(passwordJs.value))) {
-    bName.innerHTML = 'Enter your Password (from 8 characters and using numbers and upper and lower case letters)';
+    passwordJs.placeholder = "Enter your Password";
+    passwordBorder.style.borderBottom = '2px solid #FF2828';
     errors++;
     if (focusError)
       passwordJs.focus();
   }
-  else bName.innerHTML = '';
-
+  else {
+    passwordBorder.style.borderBottom = '1px solid #F2F2F2';
+  }
   function isPasswordUpper(value) {
     return uppers.test(value);
   }
@@ -156,47 +194,52 @@ function confirmPasswordJSBlur(focusError) {
   const formFirstF = document.forms.firstF;
   const confirmPasswordJS = formFirstF.elements.confirmPassword;
   const confirmPasswordJSValue = confirmPasswordJS.value;
-  let bName = document.getElementById('nameConfirmPasswordFail');
+  const confirmPasswordBorder = document.querySelector('.formText8');
   let errors = 0;
-  if (confirmPasswordJSValue.length<1 || confirmPasswordJSValue!==passwordJsValue) {
-    bName.innerHTML = "Passwords don't match";
+  if (confirmPasswordJSValue.length < 1 || confirmPasswordJSValue !== passwordJsValue) {
+    confirmPasswordJS.placeholder = "Compare passwords";
+    confirmPasswordBorder.style.borderBottom = '2px solid #FF2828';
     errors++;
     if (focusError)
       passwordJs.focus();
   }
-  else bName.innerHTML = '';
+  else {
+    confirmPasswordBorder.style.borderBottom = '1px solid #F2F2F2';
+  }
   return errors;
 }
 
 
 //submit
-formFirstF.addEventListener('submit',validateFirstF,false);
+formFirstF.addEventListener('submit', validateFirstF, false);
 
-const showS=document.querySelector('.formText11');
-showS.style.display='none';
+const showS = document.querySelector('.formText11');
+showS.style.display = 'none';
 
-function validateFirstF (eo){
-
-  
+function validateFirstF(eo) {
   eo = eo || window.event;
   let timer;
-  let errorsAll=0;
-  errorsAll+=firstNameBlur(!errorsAll);
-  errorsAll+=lastNameBlur(!errorsAll);
-  errorsAll+=nationalityChange(!errorsAll);
-  errorsAll+=emailBlur(!errorsAll);
-  errorsAll+=dateBlur(!errorsAll);
+  let errorsAll = 0;
+
+  errorsAll += firstNameBlur(!errorsAll);
+  errorsAll += lastNameBlur(!errorsAll);
+  errorsAll += nationalityChange(!errorsAll);
+  errorsAll += emailBlur(!errorsAll);
+  errorsAll += dateBlur(!errorsAll);
   if (errorsRadio == 0) {
     eo.preventDefault();
-    document.getElementById('nameRadioFail').innerHTML = 'Place a tick in the box.';
+    const radioColor1 = document.querySelector('.radio1Txt')
+    const radioColor2 = document.querySelector('.radio2Txt')
+    radioColor1.style.color = '#FF2828';
+    radioColor2.style.color = '#FF2828';
   }
-  errorsAll+=passwordJsBlur(!errorsAll);
-  errorsAll+=confirmPasswordJSBlur(!errorsAll);
+  errorsAll += passwordJsBlur(!errorsAll);
+  errorsAll += confirmPasswordJSBlur(!errorsAll);
   if (errorsAll) {
-    const shake=document.querySelector('.formInp10');
-    shake.style.animation='1.2s ease-in-out 0s normal none running trambling-animation';
-    timer=setInterval(timeAnim,3000)
-    function timeAnim(){
+    const shake = document.querySelector('.formInp10');
+    shake.style.animation = '1.2s ease-in-out 0s normal none running trambling-animation';
+    timer = setInterval(timeAnim, 3000)
+    function timeAnim() {
       timeShake();
     }
     eo.preventDefault();
@@ -205,35 +248,40 @@ function validateFirstF (eo){
     const formFirstF = document.forms.firstF;
     ajaxForm();
     let firstNameClear = formFirstF.elements.firstName;
-    firstNameClear.value='';
+    firstNameClear.value = '';
+    firstNameClear.placeholder='';
     let lastNameClear = formFirstF.elements.lastName;
-    lastNameClear.value='';
+    lastNameClear.value = '';
+    lastNameClear.placeholder='';
     let nationalityClear = formFirstF.elements.nationality;
-    nationalityClear.value='';
+    nationalityClear.value = '';
     let emailNameClear = formFirstF.elements.email;
-    emailNameClear.value='';
+    emailNameClear.value = '';
+    emailNameClear.placeholder='';
     let dataNameClear = formFirstF.elements.dateWeb;
-    dataNameClear.value='';
+    dataNameClear.value = '';
     let passwordClear = formFirstF.elements.password;
-    passwordClear.value='';
+    passwordClear.value = '';
+    passwordClear.placeholder='';
     let confirmPasswordClear = formFirstF.elements.confirmPassword;
-    confirmPasswordClear.value='';
+    confirmPasswordClear.value = '';
+    confirmPasswordClear.placeholder='';
 
-    const showS=document.querySelector('.formText11');
-    showS.style.display='';
-    timer=setInterval(timeAnim,3000)
-    function timeAnim(){
+    const showS = document.querySelector('.formText11');
+    showS.style.display = '';
+    timer = setInterval(timeAnim, 3000)
+    function timeAnim() {
       timeShow();
     }
     eo.preventDefault();
   }
-  function timeShake(){
-    const shake=document.querySelector('.formInp10');
-    shake.style.animation='';
+  function timeShake() {
+    const shake = document.querySelector('.formInp10');
+    shake.style.animation = '';
     clearInterval(timer)
   }
-  function timeShow(){
-    showS.style.display='none';
+  function timeShow() {
+    showS.style.display = 'none';
     clearInterval(timer)
   }
 }
@@ -250,7 +298,7 @@ function ajaxForm() {
   const emailValue = emailName.value;
   const dataValue = dataName.value;
   const elemsRadio = formFirstF.elements.gender;
-  const elemsRadioValue=elemsRadio.value;
+  const elemsRadioValue = elemsRadio.value;
   const passwordJsValue = passwordJs.value;
   const confirmPasswordJSValue = confirmPasswordJS.value;
 
